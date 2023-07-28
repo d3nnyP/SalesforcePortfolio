8a0c6d5ccecdece1e9688f07ae7d83c0197153ce
Dennis <denjp17@gmail.com>
2023-07-28 11:13:06 -0600

    Added Hero section LWC to Portfolio site

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import IMAGES from "@salesforce/resourceUrl/Images";
+
+export default class Hero extends LightningElement {
+  imageUrl = IMAGES + "/DennisPortfolioPic.png";
+}
