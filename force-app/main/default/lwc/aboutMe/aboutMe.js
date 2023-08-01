07f31395ffeb1413174e6c71f30592a124c6e789
Dennis <denjp17@gmail.com>
2023-07-31 23:17:39 -0600

    Added About Me Section

---
+++ force-app/main/default/lwc/aboutMe/aboutMe.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import IMAGES from "@salesforce/resourceUrl/Images";
+
+export default class AboutMe extends LightningElement {
+  aboutMeImageUrl = IMAGES + "/DennisGrouseHat.png";
+}
---
+++ force-app/main/default/lwc/highlightsCards/highlightsCards.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import IMAGES from "@salesforce/resourceUrl/Images";
+
+export default class HighlightsCards extends LightningElement {
+  adminImageUrl = IMAGES + "/DennisHeadshotSquareWeb.png";
+}
