a18a03951757b90192755b6f3b832834352f5fb0
Dennis <denjp17@gmail.com>
2023-08-01 11:08:43 -0600

    Footer and Styling Added to Portfolio

--- force-app/main/default/lwc/highlightsCards/highlightsCards.js
+++ force-app/main/default/lwc/highlightsCards/highlightsCards.js
@@ -2,5 +2,7 @@ import { LightningElement } from "lwc";
 import IMAGES from "@salesforce/resourceUrl/Images";
 
 export default class HighlightsCards extends LightningElement {
-  adminImageUrl = IMAGES + "/DennisHeadshotSquareWeb.png";
+  adminImageUrl = IMAGES + "/ORSFZC0.jpg";
+  developerImageUrl = IMAGES + "/OAI50I0.jpg";
+  projectManagementImageUrl = IMAGES + "/O9IY130.jpg";
 }
