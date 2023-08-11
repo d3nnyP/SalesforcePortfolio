c7d6fde864fe7b7ac1de6e7963abcc87f4a22c50
Dennis <denjp17@gmail.com>
2023-08-11 14:23:24 -0600

    Updated Portfolio Content Added LinkedIn Field

--- force-app/main/default/lwc/contactMeButton/contactMeButton.js
+++ force-app/main/default/lwc/contactMeButton/contactMeButton.js
@@ -5,23 +5,31 @@ export default class ContactMeButton extends LightningElement {
   dialog;
   firstName;
   lastName;
+  linkedIn;
   company;
   email;
   description;
   snackbar;
+  contactMeForm;
 
   renderedCallback() {
     this.dialog = this.template.querySelector(".contact-dialog");
     this.firstName = this.template.querySelector(".firstname");
     this.lastName = this.template.querySelector(".lastname");
+    this.linkedIn = this.template.querySelector(".linkedin");
     this.company = this.template.querySelector(".company");
     this.email = this.template.querySelector(".email");
     this.description = this.template.querySelector(".description");
     this.snackbar = this.template.querySelector("c-snackbar");
+    this.contactMeForm = this.template.querySelector(".contact-me-form");
   }
 
   showDialog() {
     this.dialog.showModal();
+  }
+
+  resetForm() {
+    this.contactMeForm.reset();
   }
 
   closeDialog() {
@@ -32,6 +40,7 @@ export default class ContactMeButton extends LightningElement {
     event.preventDefault();
     const firstnameValue = this.firstName.value;
     const lastnameValue = this.lastName.value;
+    const linkedInValue = this.linkedIn.value;
     const companyValue = this.company.value;
     const emailValue = this.email.value;
     const descriptionValue = this.description.value;
@@ -39,11 +48,13 @@ export default class ContactMeButton extends LightningElement {
     createLead({
       firstname: firstnameValue,
       lastname: lastnameValue,
+      linkedin: linkedInValue,
       company: companyValue,
       email: emailValue,
       description: descriptionValue
     }).then(() => {
       this.snackbar.showSnackbar("Your request has been received.");
+      this.resetForm();
     });
 
     this.closeDialog();
