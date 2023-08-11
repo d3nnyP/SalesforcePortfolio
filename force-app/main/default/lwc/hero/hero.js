fce19dcae1ede97bb39a08f1a57eaa3877cf4eff
Dennis <denjp17@gmail.com>
2023-08-11 17:18:18 -0600

    Color Theme Changes

--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -2,5 +2,5 @@ import { LightningElement } from "lwc";
 import IMAGES from "@salesforce/resourceUrl/Images";
 
 export default class Hero extends LightningElement {
-  imageUrl = IMAGES + "/DennisHeadshotWeb.png";
+  imageUrl = IMAGES + "/DennisHeadshotTransparent.png";
 }
