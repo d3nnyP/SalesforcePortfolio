a45544a0daa42f6599c9e7df8350b2fa509036cb
Dennis <denjp17@gmail.com>
2023-08-14 11:33:47 -0600

    Updated Color Scheme

---
+++ force-app/main/default/lwc/projects/projects.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+import IMAGES from "@salesforce/resourceUrl/Images";
+
+export default class HighlightsCards extends LightningElement {
+  adminImageUrl = IMAGES + "/ORSFZC0.jpg";
+  developerImageUrl = IMAGES + "/OAI50I0.jpg";
+  projectManagementImageUrl = IMAGES + "/O9IY130.jpg";
+}
