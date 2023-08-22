cd76368b9b921d55b93088881bfe532e508e78f7
Dennis <denjp17@gmail.com>
2023-08-22 16:56:24 -0600

    Projects content/styling + pages for the Projects

--- force-app/main/default/lwc/projects/projects.js
+++ force-app/main/default/lwc/projects/projects.js
@@ -1,33 +1,9 @@
 import { LightningElement } from "lwc";
-// import IMAGES from "@salesforce/resourceUrl/Images";
+import PROJECTS from "@salesforce/resourceUrl/Projects";
 
 export default class Projects extends LightningElement {
-  i;
-  expandGrid;
-  tabId;
-  // tabContent;
-
-  // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
-  openTab(event) {
-    this.tabId = event.target.dataset.tabid;
-    console.log(this.tabId);
-    // this.tabContent = this.template.querySelectorAll(`[data-tabid="${this.tabId}"]`);
-
-    this.expandGrid = this.template.querySelectorAll(".containerTab");
-
-    for (this.i = 0; this.i < this.expandGrid.length; this.i++) {
-      this.expandGrid[this.i].classList.remove("open");
-      this.expandGrid[this.i].classList.add("closed");
-    }
-    this.template.querySelector(`.${this.tabId}`).classList.remove("closed");
-    this.template.querySelector(`.${this.tabId}`).classList.add("open");
-  }
-  z;
-
-  closeTab(event) {
-    let tabName = event.target.dataset.closeid;
-
-    this.template.querySelector(`.${tabName}`).classList.remove("open");
-    this.template.querySelector(`.${tabName}`).classList.add("closed");
-  }
+  talentStackerImg = PROJECTS + "/projectOverviewTalentStacker.png";
+  birdAppImg = PROJECTS + "/birdAppProject.png";
+  portfolioProjectImg = PROJECTS + "/portfolioProjectImg.png";
+  catLwcImg = PROJECTS + "/catLWCImg.png";
 }
