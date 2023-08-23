bde88ac260c1ee66a88de3898201259901dd9883
Dennis <denjp17@gmail.com>
2023-08-23 15:40:34 -0600

    Added Consulting and TS Blog LWC. Fixed Nav Menu

--- force-app/main/default/lwc/navMenu/navMenu.js
+++ force-app/main/default/lwc/navMenu/navMenu.js
@@ -3,6 +3,8 @@ import ICONS from "@salesforce/resourceUrl/Icons";
 import { loadStyle } from "lightning/platformResourceLoader";
 
 export default class NavMenu extends LightningElement {
+  mediaQuery = window.matchMedia("(max-width: 600px)");
+
   renderedCallback() {
     loadStyle(this, ICONS + "/style.css");
   }
@@ -11,5 +13,17 @@ export default class NavMenu extends LightningElement {
     const navMenu = this.template.querySelector(".topnav");
 
     navMenu.classList.toggle("responsive");
+  }
+
+  /**
+   * Handles the click event on the navigation menu.
+   * If the media query matches, toggles the "responsive" class on the navigation menu.
+   */
+  handleNavMenuClicked() {
+    const navMenu = this.template.querySelector(".topnav");
+
+    if (this.mediaQuery.matches) {
+      navMenu.classList.toggle("responsive");
+    }
   }
 }
---
+++ force-app/main/default/lwc/talentStackerProjectBlog/talentStackerProjectBlog.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import BIGYEARTRACKERBLOGPOST from "@salesforce/resourceUrl/bigYearTrackerBlogPost";
+
+export default class BigYearTrackerBlog extends LightningElement {
+  heroImg = BIGYEARTRACKERBLOGPOST + "/PlaceholderBlogPost.png";
+}
