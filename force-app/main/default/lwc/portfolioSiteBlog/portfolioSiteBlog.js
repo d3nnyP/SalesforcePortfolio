a262b36df354150f05d9a89512af288eb2f265ab
Dennis <denjp17@gmail.com>
2023-09-04 16:29:52 -0600

    Design Adjustments New Placeholder Imgs

--- force-app/main/default/lwc/portfolioSiteBlog/portfolioSiteBlog.js
+++ force-app/main/default/lwc/portfolioSiteBlog/portfolioSiteBlog.js
@@ -2,5 +2,5 @@ import { LightningElement } from "lwc";
 import BIGYEARTRACKERBLOGPOST from "@salesforce/resourceUrl/bigYearTrackerBlogPost";
 
 export default class BigYearTrackerBlog extends LightningElement {
-  heroImg = BIGYEARTRACKERBLOGPOST + "/PlaceholderBlogPost.png";
+  heroImg = BIGYEARTRACKERBLOGPOST + "/ComingSoon.png";
 }
